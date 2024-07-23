f0b1e1a8374e8907a98aa212ebd9d43673fc8723
AspasiaKun <381479613@qq.com>
2024-07-23 20:06:21 +0800

    delete build/
    Signed-off-by: AspasiaKun <381479613@qq.com>

--- Assignment2/prt/build/ext_build/openexr/IlmBase/IlmThread/CMakeFiles/IlmThread.dir/IlmThreadSemaphore.cpp.o.d
+++
@@ -1,3 +0,0 @@
-ext_build/openexr/IlmBase/IlmThread/CMakeFiles/IlmThread.dir/IlmThreadSemaphore.cpp.o: \
-  /Users/bytedance/Desktop/WorkSpace/games202\ hw/Assignment2/prt/ext/openexr/IlmBase/IlmThread/IlmThreadSemaphore.cpp \
-  /Users/bytedance/Desktop/WorkSpace/games202\ hw/Assignment2/prt/build/ext_build/openexr/IlmBase/config/IlmBaseConfig.h
--- Assignment2/prt/build/ext_build/openexr/IlmBase/IlmThread/CMakeFiles/IlmThread.dir/IlmThreadSemaphorePosix.cpp.o.d
+++
@@ -1,3 +0,0 @@
-ext_build/openexr/IlmBase/IlmThread/CMakeFiles/IlmThread.dir/IlmThreadSemaphorePosix.cpp.o: \
-  /Users/bytedance/Desktop/WorkSpace/games202\ hw/Assignment2/prt/ext/openexr/IlmBase/IlmThread/IlmThreadSemaphorePosix.cpp \
-  /Users/bytedance/Desktop/WorkSpace/games202\ hw/Assignment2/prt/build/ext_build/openexr/IlmBase/config/IlmBaseConfig.h
